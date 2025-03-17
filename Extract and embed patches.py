19bf7eab844f078f800ae7bbf14d0be35242ac4a
madhurisharma88 <madhurisharma210688@gmail.com>
2025-03-17 11:11:36 +0530

    Extract and embed patches.py

---
+++ Extract and embed patches.py
@@ -0,0 +1,45 @@
+# Using tf ops since it is only used in tf.data.
+def patch_extract(images):
+    batch_size = tf.shape(images)[0]
+    patches = tf.image.extract_patches(
+        images=images,
+        sizes=(1, patch_size[0], patch_size[1], 1),
+        strides=(1, patch_size[0], patch_size[1], 1),
+        rates=(1, 1, 1, 1),
+        padding="VALID",
+    )
+    patch_dim = patches.shape[-1]
+    patch_num = patches.shape[1]
+    return tf.reshape(patches, (batch_size, patch_num * patch_num, patch_dim))
+
+
+class PatchEmbedding(layers.Layer):
+    def __init__(self, num_patch, embed_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.num_patch = num_patch
+        self.proj = layers.Dense(embed_dim)
+        self.pos_embed = layers.Embedding(input_dim=num_patch, output_dim=embed_dim)
+
+    def call(self, patch):
+        pos = ops.arange(start=0, stop=self.num_patch)
+        return self.proj(patch) + self.pos_embed(pos)
+
+
+class PatchMerging(keras.layers.Layer):
+    def __init__(self, num_patch, embed_dim):
+        super().__init__()
+        self.num_patch = num_patch
+        self.embed_dim = embed_dim
+        self.linear_trans = layers.Dense(2 * embed_dim, use_bias=False)
+
+    def call(self, x):
+        height, width = self.num_patch
+        _, _, C = x.shape
+        x = ops.reshape(x, (-1, height, width, C))
+        x0 = x[:, 0::2, 0::2, :]
+        x1 = x[:, 1::2, 0::2, :]
+        x2 = x[:, 0::2, 1::2, :]
+        x3 = x[:, 1::2, 1::2, :]
+        x = ops.concatenate((x0, x1, x2, x3), axis=-1)
+        x = ops.reshape(x, (-1, (height // 2) * (width // 2), 4 * C))
+        return self.linear_trans(x)
