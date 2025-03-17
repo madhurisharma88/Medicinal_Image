e90360af1a2e95d34fdc4a08e95cf10da36a317a
madhurisharma88 <madhurisharma210688@gmail.com>
2025-03-17 11:09:08 +0530

    Window based multi-head self-attention.py

--- Window based multi-head self-attention.py
+++ Window based multi-head self-attention.py
@@ -1,37 +1,91 @@
-def window_partition(x, window_size):
-    _, height, width, channels = x.shape
-    patch_num_y = height // window_size
-    patch_num_x = width // window_size
-    x = ops.reshape(
-        x,
-        (
-            -1,
-            patch_num_y,
-            window_size,
-            patch_num_x,
-            window_size,
-            channels,
-        ),
-    )
-    x = ops.transpose(x, (0, 1, 3, 2, 4, 5))
-    windows = ops.reshape(x, (-1, window_size, window_size, channels))
-    return windows
 
+class WindowAttention(layers.Layer):
+    def __init__(
+        self,
+        dim,
+        window_size,
+        num_heads,
+        qkv_bias=True,
+        dropout_rate=0.0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.dim = dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        self.scale = (dim // num_heads) ** -0.5
+        self.qkv = layers.Dense(dim * 3, use_bias=qkv_bias)
+        self.dropout = layers.Dropout(dropout_rate)
+        self.proj = layers.Dense(dim)
 
-def window_reverse(windows, window_size, height, width, channels):
-    patch_num_y = height // window_size
-    patch_num_x = width // window_size
-    x = ops.reshape(
-        windows,
-        (
-            -1,
-            patch_num_y,
-            patch_num_x,
-            window_size,
-            window_size,
-            channels,
-        ),
-    )
-    x = ops.transpose(x, (0, 1, 3, 2, 4, 5))
-    x = ops.reshape(x, (-1, height, width, channels))
-    return x
+        num_window_elements = (2 * self.window_size[0] - 1) * (
+            2 * self.window_size[1] - 1
+        )
+        self.relative_position_bias_table = self.add_weight(
+            shape=(num_window_elements, self.num_heads),
+            initializer=keras.initializers.Zeros(),
+            trainable=True,
+        )
+        coords_h = np.arange(self.window_size[0])
+        coords_w = np.arange(self.window_size[1])
+        coords_matrix = np.meshgrid(coords_h, coords_w, indexing="ij")
+        coords = np.stack(coords_matrix)
+        coords_flatten = coords.reshape(2, -1)
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+        relative_coords = relative_coords.transpose([1, 2, 0])
+        relative_coords[:, :, 0] += self.window_size[0] - 1
+        relative_coords[:, :, 1] += self.window_size[1] - 1
+        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        relative_position_index = relative_coords.sum(-1)
+
+        self.relative_position_index = keras.Variable(
+            initializer=relative_position_index,
+            shape=relative_position_index.shape,
+            dtype="int",
+            trainable=False,
+        )
+
+    def call(self, x, mask=None):
+        _, size, channels = x.shape
+        head_dim = channels // self.num_heads
+        x_qkv = self.qkv(x)
+        x_qkv = ops.reshape(x_qkv, (-1, size, 3, self.num_heads, head_dim))
+        x_qkv = ops.transpose(x_qkv, (2, 0, 3, 1, 4))
+        q, k, v = x_qkv[0], x_qkv[1], x_qkv[2]
+        q = q * self.scale
+        k = ops.transpose(k, (0, 1, 3, 2))
+        attn = q @ k
+
+        num_window_elements = self.window_size[0] * self.window_size[1]
+        relative_position_index_flat = ops.reshape(self.relative_position_index, (-1,))
+        relative_position_bias = ops.take(
+            self.relative_position_bias_table,
+            relative_position_index_flat,
+            axis=0,
+        )
+        relative_position_bias = ops.reshape(
+            relative_position_bias,
+            (num_window_elements, num_window_elements, -1),
+        )
+        relative_position_bias = ops.transpose(relative_position_bias, (2, 0, 1))
+        attn = attn + ops.expand_dims(relative_position_bias, axis=0)
+
+        if mask is not None:
+            nW = mask.shape[0]
+            mask_float = ops.cast(
+                ops.expand_dims(ops.expand_dims(mask, axis=1), axis=0),
+                "float32",
+            )
+            attn = ops.reshape(attn, (-1, nW, self.num_heads, size, size)) + mask_float
+            attn = ops.reshape(attn, (-1, self.num_heads, size, size))
+            attn = keras.activations.softmax(attn, axis=-1)
+        else:
+            attn = keras.activations.softmax(attn, axis=-1)
+        attn = self.dropout(attn)
+
+        x_qkv = attn @ v
+        x_qkv = ops.transpose(x_qkv, (0, 2, 1, 3))
+        x_qkv = ops.reshape(x_qkv, (-1, size, channels))
+        x_qkv = self.proj(x_qkv)
+        x_qkv = self.dropout(x_qkv)
+        return x_qkv
