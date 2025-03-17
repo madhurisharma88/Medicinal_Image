777eecb58f1eb6c6b851b3b90d811b63f0c91396
madhurisharma88 <madhurisharma210688@gmail.com>
2025-03-17 11:08:14 +0530

    Window based multi-head self-attention.py

---
+++ Window based multi-head self-attention.py
@@ -0,0 +1,37 @@
+def window_partition(x, window_size):
+    _, height, width, channels = x.shape
+    patch_num_y = height // window_size
+    patch_num_x = width // window_size
+    x = ops.reshape(
+        x,
+        (
+            -1,
+            patch_num_y,
+            window_size,
+            patch_num_x,
+            window_size,
+            channels,
+        ),
+    )
+    x = ops.transpose(x, (0, 1, 3, 2, 4, 5))
+    windows = ops.reshape(x, (-1, window_size, window_size, channels))
+    return windows
+
+
+def window_reverse(windows, window_size, height, width, channels):
+    patch_num_y = height // window_size
+    patch_num_x = width // window_size
+    x = ops.reshape(
+        windows,
+        (
+            -1,
+            patch_num_y,
+            patch_num_x,
+            window_size,
+            window_size,
+            channels,
+        ),
+    )
+    x = ops.transpose(x, (0, 1, 3, 2, 4, 5))
+    x = ops.reshape(x, (-1, height, width, channels))
+    return x
